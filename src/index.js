2ea4a0267329915c3c0ced634ef27eebb700768f
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-12-06 14:11:16 +0100

    Reasonable working point.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import _ from 'lodash';
 
 function component() {
     const element = document.createElement('div');
-
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
     return element;
 }
+
+// $( function() {
+//     $( "#sortable" ).sortable();
+// } );
 
 document.body.appendChild(component());
